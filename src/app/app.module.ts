c9b1198a889d3aeb36e376f12c833cc86abc92f5
DebbieElabonga <debbie.elabonga@student.moringaschool.com>
2021-05-22 16:31:27 +0300

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RepoComponent } from './repo/repo.component';
+import { SearchComponent } from './search/search.component';
+import { UserComponent } from './user/user.component';
+import { UserserviceComponent } from './userservice/userservice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RepoComponent,
+    SearchComponent,
+    UserComponent,
+    UserserviceComponent
   ],
   imports: [
     BrowserModule,
