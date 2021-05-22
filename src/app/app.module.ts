7836f87a563e5caa7e8684bea9a7817fb7f3340b
DebbieElabonga <debbie.elabonga@student.moringaschool.com>
2021-05-22 16:32:35 +0300

    custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { RepoComponent } from './repo/repo.component';
 import { SearchComponent } from './search/search.component';
 import { UserComponent } from './user/user.component';
 import { UserserviceComponent } from './userservice/userservice.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { UserserviceComponent } from './userservice/userservice.component';
     RepoComponent,
     SearchComponent,
     UserComponent,
-    UserserviceComponent
+    UserserviceComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
